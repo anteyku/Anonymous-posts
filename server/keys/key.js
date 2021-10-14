f4b6a0a457a2d9740b13e6cfc34e3aba2dd0f226
Anteyku Company <68657278+anteyku@users.noreply.github.com>
2021-10-14 13:20:57 +0300

    Delet
    Deleted key

--- server/keys/key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoUri: `mongodb+srv://new_user:Lw9SGYJ7gF3g7yyGgy3Z@cluster0.s0hts.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-    url: `http://localhost:5000`,
-    email: `annaparmizanova@gmail.com`,
-    secret: `kavaki shit deyam men`
-}
